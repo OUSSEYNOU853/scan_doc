b2656959a6a8b67acb0c4ff7fadedd33ffde9568
ousseynou diedhiou <diedhiouousseynou53@gmail.com>
2025-05-17 23:09:43 +0000

    modification du fichier app

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def scan_document():
             os.unlink(tmp.name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
